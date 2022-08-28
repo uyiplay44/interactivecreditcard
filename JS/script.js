0d866f29d2b4dfb3787314479750cd6de3eca187
Oyenwen Ugiagbe <harvest@Oyenwens-MacBook-Air.local>
2022-08-28 18:58:56 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+const formList = document.querySelector('.card-holder')
+const inputText = document.getElementById('card')
+console.log(inputText);
+const textInput = document. querySelectorAll('.input-box')
+
+// console.dir(document)
